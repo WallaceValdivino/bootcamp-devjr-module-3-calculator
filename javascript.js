4755f76feca937cf5ec3c008ec235a922b63bab6
Wallace <wallace.silva35@fatec.sp.gov.br>
2023-07-14 16:09:54 -0300

    closes #4 - Implementar as outras operações

--- javascript.js
+++ javascript.js
@@ -1,8 +1,21 @@
-function calc(op){
-var num1 = parseFloat(document.getElementById("num1").value);
-var num2 = parseFloat(document.getElementById("num2").value);
+function calc(op) {
+    var num1 = parseFloat(document.getElementById("num1").value);
+    var num2 = parseFloat(document.getElementById("num2").value);
+    var output = 0;
 
-var output = num1 + num2;
+    var output = num1 + num2;
+    switch (op) {
+        case '+':
+            output = num1 + num2; break;
 
-document.getElementById("output").value = output;
+        case '-':
+            output = num1 - num2; break;
+
+        case '/':
+            output = num1 / num2; break;
+
+        case '*':
+            output = num1 * num2; break;
+    }
+    document.getElementById("output").value = output;
 }
